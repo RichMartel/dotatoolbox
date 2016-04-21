2a649d33a04c108dd5156c1dc80c24d8e2f80ba2
Rich Martel <richmartel@gmail.com>
2016-04-21 18:20:26 -0400

    you can now input steam id

--- webroot/app/friend/friend.js
+++ webroot/app/friend/friend.js
@@ -7,9 +7,9 @@
 		constructor: [ng.http.Http, function(http) {
 			ga('set', 'page', '/friend');
 			ga('send', 'pageview');
-			Sortable.init();
+			this.http = http;
 			this.player = {
-				steamid: '76561198031077846'// rich: '76561197968069993'
+				steamid: '' // rich: '76561197968069993' mike: '76561198031077846'
 			};
 			this.friends = [];
 			var corsproxies = [
@@ -34,7 +34,7 @@
 					url: 'http://cors-anywhere.herokuapp.com/'
 				}
 			];
-			var corsproxy = corsproxies[4].url;
+			this.corsproxy = corsproxies[4].url;
 			// for (var i = 0; i < corsproxies.length; i++) {
 			// 	http.get(corsproxies[i].home).map(res => {
 			// 		if (res.ok) {
@@ -50,15 +50,17 @@
 			// 		console.log('data:', data);
 			// 	});
 			// }
-			var apiurl = {
+			this.apiurl = {
 				friends: 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=6A2645535F4019083D3EB51D0002B7A3&steamid=',
 				player: 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=6A2645535F4019083D3EB51D0002B7A3&steamids=',
 				dota2: 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=6A2645535F4019083D3EB51D0002B7A3&include_appinfo=1&include_played_free_games=1&appids_filter[0]=570&steamid='
 			};
-			http.get(corsproxy + apiurl.friends + this.player.steamid).map(res => res.json())
+		}],
+		get: function() {
+			this.http.get(this.corsproxy + this.apiurl.friends + this.player.steamid).map(res => res.json())
 				.subscribe(data => {
 					this.friends = data.friendslist.friends;
-					http.get(corsproxy + apiurl.player + this.player.steamid).map(res => res.json())
+					this.http.get(this.corsproxy + this.apiurl.player + this.player.steamid).map(res => res.json())
 						.subscribe(data => {
 							var player = data.response.players[0];
 							this.player.avatar = player.avatar;
@@ -76,16 +78,16 @@
 							this.player.locstatecode = player.locstatecode;
 							this.player.loccityid = player.loccityid;
 						});
-					http.get(corsproxy + apiurl.dota2 + this.player.steamid).map(res => res.json())
+					this.http.get(this.corsproxy + this.apiurl.dota2 + this.player.steamid).map(res => res.json())
 						.subscribe(data => {
-							this.player.playtime_forever = data.response.games[0].playtime_forever;
-							this.player.playtime_2weeks = data.response.games[0].playtime_2weeks;
+							this.player.playtime_forever = Math.round(data.response.games[0].playtime_forever / 60);
+							this.player.playtime_2weeks = Math.round(data.response.games[0].playtime_2weeks / 60);
 						});
 					for (var i = 0; i < this.friends.length; i++) {
 						var d = new Date(0);
 						d.setUTCSeconds(this.friends[i].friend_since);
 						this.friends[i].friend_since = d;
-						http.get(corsproxy + apiurl.player + this.friends[i].steamid).map(res => res.json())
+						this.http.get(this.corsproxy + this.apiurl.player + this.friends[i].steamid).map(res => res.json())
 						.subscribe(data => {
 							var player = data.response.players[0];
 							var friend = _.find(this.friends, {steamid: player.steamid});
@@ -105,7 +107,7 @@
 							friend.loccityid = player.loccityid;
 							Sortable.init();
 						});
-						http.get(corsproxy + apiurl.dota2 + this.friends[i].steamid).map(res => {
+						this.http.get(this.corsproxy + this.apiurl.dota2 + this.friends[i].steamid).map(res => {
 							var steamid = res.url.replace(/.*steamid=(.*)/, '$1');
 							var res = res.json();
 							res.steamid = steamid;
@@ -113,12 +115,19 @@
 						})
 						.subscribe(data => {
 							var friend = _.find(this.friends, {steamid: data.steamid});
-							friend.playtime_forever = data.response.games[0].playtime_forever;
-							friend.playtime_2weeks = data.response.games[0].playtime_2weeks;
+							if (friend && data.response.games[0]) {
+								friend.playtime_forever = Math.round(data.response.games[0].playtime_forever / 60);
+								if (friend.playtime_forever > 0 && !data.response.games[0].playtime_2weeks) {
+									friend.playtime_2weeks = 0;
+								} else {
+									friend.playtime_2weeks = Math.round(data.response.games[0].playtime_2weeks / 60);
+								}
+							}
+							Sortable.init();
 						});
 					}
 				});
-		}]
+		}
 	});
 
 })(window.app || (window.app = {}));
